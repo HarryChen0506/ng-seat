45b7278e6b3676ccdb276828d221e2f0361fb42b
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2017-08-28 14:34:36 +0800

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngSeatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngSeatApp
+ */
+angular.module('ngSeatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
